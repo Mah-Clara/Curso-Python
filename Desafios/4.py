9bb55283edbd853c4a2969ac1dacc01e8b132966
Mah-Clara <maria.clara1212@hotmail.com>
2022-05-16 22:13:15 -0300

    Criei Desafio 4.py
    Desafio 4 feito no curso de Python.

---
+++ Desafios/4.py
@@ -0,0 +1,10 @@
+tp = input("Digite algo: ")
+
+print('O tipo primitivo desse valor é ', type(tp))
+print('Só tem espaço? ', tp.isspace())
+print('É um número? ', tp.isnumeric())
+print('É um alfabeto? ', tp.isalpha())
+print('É um alfanumérico? ', tp.isalnum())
+print('Está em maiúsculas? ', tp.isupper())
+print('Está em minúsculas? ', tp.islower())
+print('Está capitalizada? ', tp.istitle())
