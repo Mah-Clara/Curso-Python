ac092c2d29289f0b8c6ec830435b9355f56be209
Mah-Clara <maria.clara1212@hotmail.com>
2022-05-21 19:09:02 -0300

    Adicionando a descrição do desafio

--- desafios/mundo 1/013.py
+++ desafios/mundo 1/013.py
@@ -1,3 +1,5 @@
+# Desafio 013 - Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
 sl = float(input('Qual é o valor do seu salário? '))
 au = sl + (sl * 15 / 100)
 
