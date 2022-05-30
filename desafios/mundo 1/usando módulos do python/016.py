ed10a16fde68c81587f7277e4bd049aacccb6a5d
Mah-Clara <maria.clara1212@hotmail.com>
2022-05-30 18:25:18 -0300

    Desafio 16 adicionado

---
+++ desafios/mundo 1/usando módulos do python/016.py
@@ -0,0 +1,11 @@
+# Desafio 016 - Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira
+
+# Sem importação:
+# n = float(input('Digite um número: '))
+# print('O número {} tem a parte inteira {:.0f}.'.format(n, n))
+
+# Com importação:
+from math import trunc
+
+n = float(input('Digite um número: '))
+print('O número {} tem a parte inteira {}.'.format(n, trunc(n)))
