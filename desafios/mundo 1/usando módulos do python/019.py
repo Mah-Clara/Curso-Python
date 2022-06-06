c5fb6fc1e15bf50f8ac20356439d5b34941f31cb
Mah-Clara <maria.clara1212@hotmail.com>
2022-06-05 23:59:08 -0300

    Create 019.py

---
+++ desafios/mundo 1/usando módulos do python/019.py
@@ -0,0 +1,11 @@
+# Um professor quer sortear seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+
+import random
+
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+random()
+
+print('O aluno escolhido foi {}'.format(random))
