8e78e8baf1d85341d307c6d181db28e66c291bc2
Mah-Clara <maria.clara1212@hotmail.com>
2022-06-09 23:26:00 -0300

    Primeiros passos com o Python

---
+++ desafios/mundo 1/01 primeiros passos com o python/002.py
@@ -0,0 +1,5 @@
+# Desafio 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Qual é seu nome? ')
+
+print('Seja bem-vindo(a), ' + nome + '! Prazer em te conhecer.')
