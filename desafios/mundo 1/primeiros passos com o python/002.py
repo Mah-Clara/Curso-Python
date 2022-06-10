d79b4205a8abab4bf8bf1f88ec6ceff3158af9c5
Mah-Clara <maria.clara1212@hotmail.com>
2022-06-09 23:27:28 -0300

    Nome da pasta alterada (01)

--- desafios/mundo 1/primeiros passos com o python/002.py
+++
@@ -1,5 +0,0 @@
-# Desafio 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
-
-nome = input('Qual é seu nome? ')
-
-print('Seja bem-vindo(a), ' + nome + '! Prazer em te conhecer.')
