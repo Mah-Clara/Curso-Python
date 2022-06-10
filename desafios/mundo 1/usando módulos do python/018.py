745ee87ae8be8ab0cc29edd4d88b4faee49b2dbc
Mah-Clara <maria.clara1212@hotmail.com>
2022-06-09 23:28:12 -0300

    Nome da pasta alterada (03)

--- desafios/mundo 1/usando módulos do python/018.py
+++
@@ -1,9 +0,0 @@
-# Desafio 018 - Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
-
-from math import radians, sin, cos, tan
-an = float(input('Digite o ângulo: '))
-sen = sin(radians(an))
-cos = cos(radians(an))
-tan = tan(radians(an))
-
-print('O ângulo de {:.2f} tem:\nSeno de {:.2f}\nCosseno de {:.2f}\nTangente de {:.2f}'.format(an, sen, cos, tan))
--- desafios/mundo 1/usando módulos do python/021.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load('021.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
